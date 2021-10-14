fd22cc20582570f65c6bf8500e3d105c638a5e63
777shetty <shetty.chetu7@gmail.com>
2021-10-15 01:01:35 +0530

    ADDED:Display employee storage from local storage

--- js/payroll_home.js
+++ js/payroll_home.js
@@ -1,35 +1,44 @@
-window.addEventListener('DOMContentLoaded',(event) => {
+let empPayrollList;
+window.addEventListener('DOMContentLoaded',(event) =>{
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
-    });
+});
 
-    const createInnerHtml = () => {
-      const headerHtml = ` 
-        <th></th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Department</th>
-        <th>Salary</th>
-        <th>Start Date</th>
-        <th>Actions</th>
-      `;
-      const innerHtml = `${headerHtml}
-      <tr>
-          <td>
-          <img class="profile" alt="" src="../assets/profile-images/Ellipse -3.png">
-          </td>
-          <td>Anthony Stark</td>
-          <td>Male</td>
-          <td><div class="dept-label">Finance</div>
-              <div class="dept-label">Engineer</div></td>
-          <td>499999</td>
-          <td>1 Jan 2016</td>
-          <td>
-          <img id="1" onclick="remove(this)" alt="delete" 
-                  src="../assets/icons/delete-black-18dp.svg">
-          <img id="1" alt="edit" onclick="update(this)"
-                  src="../assets/icons/create-black-18dp.svg">
-          </td>
-      </tr>
-      `;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+const createInnerHtml = () => {
+    if(empPayrollList.length == 0) return;
+    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+    let innerHtml = `${headerHtml}`;
+    for (const empPayrollData of empPayrollList){
+        const date = new Date(empPayrollData._startDate);
+        const month = date.toLocaleString('default', { month: 'short' });
+        let dateString = date.getDate()+" "+month+" "+ date.getFullYear();
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile"  src="${empPayrollData._profilePic}" alt=""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${(getDeptHtml(empPayrollData._department))}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${dateString}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img name="${empPayrollData._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
-    } 
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+    }
+    return deptHtml;
+}
